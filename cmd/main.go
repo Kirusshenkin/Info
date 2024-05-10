bf5a952a4fad81ba3101a6618fec9b1276a5d5e5
kirill mitin <kirill.mitin34@gmail.com>
2024-05-10 07:18:02 +0300

    add docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"cryptoInfo/internal/database"
-	"cryptoInfo/internal/handler"
+	"cryptoApi/internal/database"
+	"cryptoApi/internal/handler"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,4 +15,11 @@ func main() {
     http.HandleFunc("/", handler.HomeHandler)
     http.HandleFunc("/prices", handler.PricesHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
+
+		cwd, _ := os.Getwd()
+		files, _ := ioutil.ReadDir("./")
+		log.Printf("Current working directory: %s", cwd)
+		for _, f := range files {
+				log.Println(f.Name())
+		}
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,16 +1,11 @@
 package handler
 
 import (
-	"cryptoInfo/internal/prices"
+	"cryptoApi/internal/prices"
 	"html/template"
+	"log"
 	"net/http"
 )
-
-var tmpl = template.Must(template.ParseFiles("templates/index.html"))
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl.Execute(w, nil)
-}
 
 func PricesHandler(w http.ResponseWriter, r *http.Request) {
     pricesData, err := prices.GetCryptoPrices()
@@ -20,4 +15,32 @@ func PricesHandler(w http.ResponseWriter, r *http.Request) {
     }
     w.Header().Set("Content-Type", "application/json")
     w.Write(pricesData)
+}
+
+var tmpl *template.Template
+
+func init() {
+    var err error
+    tmpl, err = template.ParseFiles("templates/index.html")
+    if err != nil {
+        log.Fatalf("Failed to parse template: %v", err)
+    }
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    priceData, err := prices.GetCryptoPrices()
+    if err != nil {
+        http.Error(w, "Unable to fetch prices", http.StatusInternalServerError)
+        return
+    }
+
+    data := struct {
+        Price string
+    }{
+        Price: string(priceData),
+    }
+
+    if err := tmpl.Execute(w, data); err != nil {
+        http.Error(w, "Error rendering template", http.StatusInternalServerError)
+    }
 }
